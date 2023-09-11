15bbb90cebbf4f6807f1b2d31d27465cdd7eae21
Juan Ignacio Campos Martí <104795448+N4choCM@users.noreply.github.com>
2023-09-11 09:46:58 +0200

    ✨ INIT: Project initialized

---
+++ src/main/java/com/nacho/campos/dev/GamesBackendJavaSpring/GamesBackendJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.nacho.campos.dev.GamesBackendJavaSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesBackendJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesBackendJavaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nacho/campos/dev/GamesBackendJavaSpring/GamesBackendJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nacho.campos.dev.GamesBackendJavaSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesBackendJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
